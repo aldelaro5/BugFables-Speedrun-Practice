e51be7a9ac03a802621673e52adb8662188c63af
Alexandre Cabana <58051353+Lyght-1@users.noreply.github.com>
2021-02-19 22:39:38 -0500

    Make the toggle collision work
    -fix the collision bug
    -fix getting stuck on a wall for a sec even with collision off.

--- Patches/EntityControl.cs
+++ Patches/EntityControl.cs
@@ -1,21 +1,16 @@
 ﻿using HarmonyLib;
 using SpeedrunPractice.Extensions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace SpeedrunPractice.Patches
 {
-  [HarmonyPatch(typeof(EntityControl), "Unfix", new Type[] { typeof(bool) })]
-  public class PatchEntityControlUnfix
-  {
-    static bool Prefix(EntityControl __instance, bool force)
+    [HarmonyPatch(typeof(EntityControl), "Unfix", new Type[] { typeof(bool) })]
+    public class PatchEntityControlUnfix
     {
-      if (!MainManager_Ext.toggleCollision && __instance.name == "Player 0")
-        return true;
-
-      return false;
+        static void Postfix(EntityControl __instance)
+        {
+            if (__instance.playerentity)
+                __instance.ccol.enabled = !MainManager_Ext.toggleCollision;
+        }
     }
-  }
 }
